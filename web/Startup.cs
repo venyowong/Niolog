98a88015cef8784e82f0e95c111f888d4e6b82be
venyowong <venyowong@163.com>
2019-08-31 00:30:32 +0800

    remove extcore

--- web/Startup.cs
+++ web/Startup.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using ExtCore.WebApplication.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,22 +14,17 @@ namespace Niolog.Web
 {
     public class Startup
     {
-        private string extensionsPath;
-
         public IConfiguration Configuration { get; }
 
         public Startup(IHostingEnvironment hostingEnvironment, IConfiguration configuration)
         {
             Configuration = configuration;
-            this.extensionsPath = Path.Combine(hostingEnvironment.ContentRootPath, configuration["Extensions:Path"]);
         }
         
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddExtCore(this.extensionsPath, true);
-
             services.AddMvc();
 
             services.AddOptions();
@@ -64,8 +58,6 @@ namespace Niolog.Web
             app.UseStaticFiles();
             app.UseCookiePolicy();
             app.UseCors("Default");
-
-            app.UseExtCore();
 
             app.UseMvc(routes =>
             {
